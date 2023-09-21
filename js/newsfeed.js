a86d07bd345d23f5a2b44d3282d5c1a749364af5
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2023-09-22 00:00:04 +0530

    added new credentials and function

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -1,7 +1,17 @@
-var credential = {
-  user: "admin",
-  password: "admin123",
-};
+var credential = [
+  {
+    user: "admin",
+    password: "admin123",
+  },
+  {
+    user: "admin1",
+    password: "admin111",
+  },
+  {
+    user: "admin2",
+    password: "admin222",
+  },
+];
 
 var news = [
   {
@@ -12,13 +22,36 @@ var news = [
     user: "user2",
     timeline: "learning objects in javascript today",
   },
+  {
+    user: "user3",
+    timeline: "learning something new daily about javascript",
+  },
+  {
+    user: "user4",
+    timeline: " javascript today validations",
+  },
 ];
 
 var userNamePrompt = prompt("Enter username");
 var passwordPrompt = prompt("Enter password");
 
+function isValidUser(user,password) {
+  for (let i in credential) {
+    if (credential[i].user === user && credential[i].password === password) {
+      return true;
+    }
+  }
+  return false;
+}
+
 function checkCredentials(user, pass) {
-  if (user === credential.user && pass === credential.password) {
+  // if (user === credential.user && pass === credential.password) {
+  //   console.log(news);
+  // } else {
+  //   alert("Invalid credentials");
+  // }
+
+  if (isValidUser(user, pass)) {
     console.log(news);
   } else {
     alert("Invalid credentials");
@@ -26,5 +59,3 @@ function checkCredentials(user, pass) {
 }
 
 checkCredentials(userNamePrompt, passwordPrompt);
-
-
