5fb461ec223e6a81755defa0b9c577757d3f3f7c
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2023-09-21 23:37:29 +0530

    added for in loop

--- js/Loops.js
+++ js/Loops.js
@@ -1,17 +1,22 @@
 // for Loop
 const arr = ["gym", "workout meal", "travel to office", " work"];
 for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i], i)
+  console.log(arr[i], i);
 }
-console.log("---------------------------------------------------")
+console.log("---------------------------------------------------");
 
 // forEach Loop
-arr.forEach(function(ar,i){
-  console.log(ar,i)
-})
+arr.forEach(function (ar, i) {
+  console.log(ar, i);
+});
 
 // another forEach Loop example
-function callFunc(ar,i){
-    console.log(ar,i)
+function callFunc(ar, i) {
+  console.log(ar, i);
 }
-arr.forEach(callFunc)
+arr.forEach(callFunc);
+
+// for in Loop
+for (let i in arr) {
+  console.log(arr[i], "for in");
+}
