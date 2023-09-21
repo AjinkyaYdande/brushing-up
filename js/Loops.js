8ce4c839684bc905a0d11e538170dce8bec7a71a
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2023-09-21 23:33:30 +0530

    for and forEach loop

---
+++ js/Loops.js
@@ -0,0 +1,17 @@
+// for Loop
+const arr = ["gym", "workout meal", "travel to office", " work"];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i], i)
+}
+console.log("---------------------------------------------------")
+
+// forEach Loop
+arr.forEach(function(ar,i){
+  console.log(ar,i)
+})
+
+// another forEach Loop example
+function callFunc(ar,i){
+    console.log(ar,i)
+}
+arr.forEach(callFunc)
