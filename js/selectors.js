bcbbbb201b18285cbacca63a5e2da11c7cfb65c6
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2023-09-24 14:24:19 +0530

    learned styling and innerHTML

---
+++ js/selectors.js
@@ -0,0 +1,20 @@
+// document.getElementById('first');
+
+// document.getElementsByClassName("second");
+
+// document.getElementsByClassName("second")[0];
+
+// document.getElementsByTagName("h1");
+
+// document.querySelector("li").getAttribute("random")
+
+// document.querySelector("li").setAttribute("random","1000");
+
+
+// -------------------- Styling
+
+// document.querySelector("h1").style.background = "gray"
+
+// 
+
+// document.querySelector("h1").innerHTML="<strong>!!!</strong>";
