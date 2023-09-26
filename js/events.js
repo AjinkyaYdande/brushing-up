60b8245664cafae9f2d4353f4bed24e0cb94ffe5
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2023-09-26 23:49:39 +0530

    implemented createelement and appendChild

--- js/events.js
+++ js/events.js
@@ -1,6 +1,17 @@
-var button = document.getElementsByTagName("button")[0];
-console.log(button,"button")
+// var button = document.getElementsByTagName("button")[0];
+// console.log(button,"button")
 
-button.addEventListener("mouseenter",function(){
-    console.log("clicked")
+// button.addEventListener("mouseenter",function(){
+//     console.log("clicked")
+// })
+
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+button.addEventListener("click",function(){
+    var li = document.createElement("li");
+    console.log(input.value)
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
 })
