488089913fd5854581be31b904a4c850e4fc09f5
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2023-09-27 23:25:27 +0530

    added functions

--- js/events.js
+++ js/events.js
@@ -9,20 +9,25 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
+function inputLength() {
+  return input.value.length;
+}
+
+function createElementList() {
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
+}
+
 button.addEventListener("click", function () {
-  if (input.value.length > 0) {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = ""
+  if (inputLength() > 0) {
+    createElementList();
   }
 });
 
 input.addEventListener("keypress", function (event) {
-    if (input.value.length > 0 && event.keyCode === 13) {
-      var li = document.createElement("li");
-      li.appendChild(document.createTextNode(input.value));
-      ul.appendChild(li);
-      input.value = ""
-    }
-  });
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createElementList();
+  }
+});
