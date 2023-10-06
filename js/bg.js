3eac3c027dab921642eac050772fca7e0540dbcf
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2023-10-07 00:00:24 +0530

    added eventlistener

--- js/bg.js
+++ js/bg.js
@@ -1,13 +1,14 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var body = document.getElementById('gradient')
 
-color1.addEventListener('input',function(){
-    console.log(color1,'color1');
-})
+color1.addEventListener('input',bg)
 
-color2.addEventListener('input',function(){
-    console.log(color2,'color2')
-})
+color2.addEventListener('input',bg)
 
-console.log(css,'css');
+function bg(){
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+}
+
+
