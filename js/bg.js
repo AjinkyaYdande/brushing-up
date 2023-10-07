06a58cf73443ff181d55999bdb5729dc1424b99d
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2023-10-07 23:43:09 +0530

    added oninput attribute in input type

--- js/bg.js
+++ js/bg.js
@@ -3,9 +3,9 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById('gradient')
 
-color1.addEventListener('input',bg)
+// color1.addEventListener('input',bg)
 
-color2.addEventListener('input',bg)
+// color2.addEventListener('input',bg)
 
 function bg(){
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
