0b85b8f1d30e3593e522b855d2c07429b9cac6dc
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2023-10-11 23:30:15 +0530

    Default parameters & template literals

--- js/obj.js
+++ js/obj.js
@@ -1,8 +1,14 @@
 // Object properties
 
-const name = "Zinc";
+// const name = "Zinc";
 
-const obj = {
-    name : "test",
-    [1+2] : "wierd object property define"
+// const obj = {
+//     name : "test",
+//     [1+2] : "wierd object property define"
+// }
+
+function test(name='',age=20){
+    return `Hello ${name} your age seems ${age}`
 }
+
+test()
